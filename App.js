1aac03e4a7c1829d8eef3f699c5b91cc518c4199
Athira <athiraramesh1501@gmail.com>
2024-11-19 11:08:37 +0530

    class 2

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "hello"),
-        React.createElement("h1", {}, "I am Athiraaa")
-    ]
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    [React.createElement("h1", {}, "hello"),
-        React.createElement("h1", {}, "I am Athiraaa")
-    ]
-  )
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-]
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hellosssssssssss"),
+    React.createElement("h1", {}, "I am Athiraaa"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "hello"),
+    React.createElement("h1", {}, "I am Athiraaa"),
+  ]),
+]);
 
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
